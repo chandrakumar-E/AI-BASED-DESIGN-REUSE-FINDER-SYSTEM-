a0e983e8244e3904fef09dbb3cdf3520c8cdc872
chandra kumar <c19062004@gmail.com>
2026-02-13 09:59:59 +0530

    Auth repository created

---
+++ AI BASED DESIGN REUSE FINDER/BACKEND-SPRINGBOOT/src/main/java/com/fyp/plm/auth/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.fyp.plm.auth.repository;
+
+import com.fyp.plm.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
