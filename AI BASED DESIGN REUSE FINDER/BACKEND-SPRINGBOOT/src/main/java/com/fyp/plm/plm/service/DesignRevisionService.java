469a295cc96225268317ce814cd2b980d5e81d86
chandra kumar <c19062004@gmail.com>
2026-02-13 12:37:14 +0530

    plm service created

---
+++ AI BASED DESIGN REUSE FINDER/BACKEND-SPRINGBOOT/src/main/java/com/fyp/plm/plm/service/DesignRevisionService.java
@@ -0,0 +1,7 @@
+package com.fyp.plm.plm.service;
+
+import com.fyp.plm.plm.entity.DesignRevision;
+
+public interface DesignRevisionService {
+    DesignRevision createRevision(DesignRevision revision);
+}
